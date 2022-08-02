452a62f5cd6e474780d263bd8f0c50ce246f20cb
gl0by <ehor.tsvietkov@gmail.com>
2022-08-03 00:22:29 +0100

    Added writing in the data file

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const https = require('https');
+const https = require('node:https');
 const fs = require('fs');
 
 https.get('https://api.openweathermap.org/data/2.5/weather?lat=35&lon=130&appid=a2447cc75255e17ec76e48f8e41200b9', (res) => {
-    console.log(res);
-})
+    console.log('statusCode:', res.statusCode);
+    console.log('headers:', res.headers);
+
+    res.on('data', (d) => {
+        fs.writeFile('data.json', d, (err) => {
+            if (err) {
+                console.log('error:', err);
+                return
+            }
+        });
+    });
+
+}).on('error', (e) => {
+    console.error(e);
+  });
