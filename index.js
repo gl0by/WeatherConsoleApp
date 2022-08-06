9ffd59886ca7437a974201957c58ef3738c7b047
gl0by <ehor.tsvietkov@gmail.com>
2022-08-06 01:29:25 +0100

    Added console.log
    Logic: data from "data.json" transferring to bank and then from there going to console

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const https = require('node:https');
 const fs = require('fs');
 
+let bank = new Object();
+
 https.get('https://api.openweathermap.org/data/2.5/weather?lat=35&lon=130&appid=a2447cc75255e17ec76e48f8e41200b9', (res) => {
     console.log('statusCode:', res.statusCode);
-    console.log('headers:', res.headers);
 
     res.on('data', (d) => {
-        fs.writeFile('data.json', d, (err) => {
+        fs.writeFileSync('data.json', d, (err) => {
             if (err) {
                 console.log('error:', err);
                 return
@@ -17,3 +18,11 @@ https.get('https://api.openweathermap.org/data/2.5/weather?lat=35&lon=130&appid=
 }).on('error', (e) => {
     console.error(e);
   });
+
+bank = JSON.parse(fs.readFileSync('data.json'));
+
+console.log(
+    "CITY: " + bank.name,
+    "Temperature: " + ((bank.main.temp - 273.15).toFixed(2)),
+    "Feels like: " + ((bank.main.feels_like - 273.15).toFixed(2))
+);
